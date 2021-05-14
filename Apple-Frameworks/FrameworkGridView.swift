bd38c96790ec94f98f5ae7486895f0ac0ee8f839
Frantisek Varadzin <varadzin@gmail.com>
2021-05-14 07:36:16 +0200

    learning Swift UI

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Frantisek Varadzin on 14/05/2021.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
